7481158e37655ce7986c4611f846aea96f182e76
IgorBia <igor.bialas@gmail.com>
2025-05-12 17:40:20 +0200

    Little error handling updates to stock_scheduler_service

--- stock_scheduler_service/internal/fetch.go
+++ stock_scheduler_service/internal/fetch.go
@@ -9,22 +9,45 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-func fetchCandleData(symbol string, interval string) [][]interface{} {
+func buildURL(base string, symbol string, interval string) (*url.URL, error) {
 
-	baseURL := "https://api.binance.com/api/v3/klines"
+	if base == "" {
+		log.Error("Base URL is empty")
+	}
+	if symbol == "" {
+		log.Error("Symbol is empty")
+	}
+	if interval == "" {
+		log.Error("Interval is empty")
+	}
+
+	if base == "" || symbol == "" || interval == "" {
+		return nil, nil
+	}
+
 	params := url.Values{}
 	params.Add("symbol", symbol)
 	params.Add("interval", interval)
 	params.Add("limit", "1")
 
-	parsedURL, err := url.Parse(baseURL)
+	parsedURL, err := url.Parse(base)
 	if err != nil {
 		log.WithError(err).Error("Failed to parse URL")
-		return nil
+		return nil, err
 	}
 	parsedURL.RawQuery = params.Encode()
 
-	response, err := http.Get(parsedURL.String())
+	return parsedURL, nil
+}
+
+func fetchCandleData(symbol string, interval string) [][]interface{} {
+
+	apiURL, err := buildURL("https://api.binance.com/api/v3/klines", symbol, interval)
+	if apiURL == nil {
+		log.Error("Failed to build URL")
+		return nil
+	}
+	response, err := http.Get(apiURL.String())
 	if err != nil {
 		log.WithError(err).Error("Failed request")
 		return nil
