b43207efb2e81db949461fbd501e96e8e35ae70c
IgorBia <igor.bialas@gmail.com>
2025-05-12 13:52:25 +0200

    Spring boot application initialized.

---
+++ stock_api_service/src/test/java/com/stockviewer/stockapi/StockapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockviewer.stockapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
