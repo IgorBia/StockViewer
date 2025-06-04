e8e7291386224df6d1b10dd860ad607736895f3e
IgorBia <igor.bialas@gmail.com>
2025-06-04 17:53:28 +0200

    Docker setup ready, Dockerfiles, docker-compose and init.sql added, modified spring application.properties to run on port 8080.

--- stock_api_service/src/main/java/com/stockviewer/stockapi/config/CorsConfig.java
+++ stock_api_service/src/main/java/com/stockviewer/stockapi/config/CorsConfig.java
@@ -14,11 +14,12 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500", "http://localhost:3000")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
                         .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowCredentials(false);
             }
         };
     }
 }
+
